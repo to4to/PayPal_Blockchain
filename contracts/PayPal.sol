9d9581a495af3db2a45f2f2703b2a0e407e86762
Himanshu KumarSingh <to4to@Himanshus-MacBook-Air.local>
2023-09-18 22:14:51 +0530

    Structs + mappings Added

--- contracts/PayPal.sol
+++ contracts/PayPal.sol
@@ -15,9 +15,31 @@ constructor() {
 
 //Struct mapping for request transaction and name
 
+struct request{
+    address requestor;
+    uint amount;
+    string message;
+    string name; 
+}
+
+struct sendReceive{
+string action;
+uint amount;
+string message;
+address otherPartyAddress;
+string otherPArtyName;
+
+}
+
+struct userName{
+    string name;
+    bool hasName;
+}
 
 
-
+mapping(address=>userName) names;
+mapping(address=>request[]) requests;
+mapping(address=>sendReceive[]) history;
 //Adding a name to wallet address
 
 
