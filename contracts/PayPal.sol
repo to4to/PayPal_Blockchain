2bf07776c39484ba1ece87ad51ebb3795b1bc5a7
Himanshu KumarSingh <to4to@Himanshus-MacBook-Air.local>
2023-09-18 21:40:40 +0530

    Comments for functions

---
+++ contracts/PayPal.sol
@@ -0,0 +1,45 @@
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract PayPal{
+
+//Defining Owner Of Contract
+
+
+
+//Struct mapping for request transaction and name
+
+
+
+
+//Adding a name to wallet address
+
+
+
+
+//Creating a request
+
+
+
+
+
+//Paying To Request
+
+
+
+
+
+
+//Get All all request send to user
+
+
+
+
+//git history of transaction of user
+
+
+
+
+}
