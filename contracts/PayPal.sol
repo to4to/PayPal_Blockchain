137050fb76686ca5965096d98a548e900bd3c03f
to4to <justjustifying@outlook.com>
2023-09-20 02:35:07 +0530

    Satrting payRequest Function

--- contracts/PayPal.sol
+++ contracts/PayPal.sol
@@ -33,44 +33,40 @@ contract PayPal {
     mapping(address => userName) names;
     mapping(address => request[]) requests;
     mapping(address => sendReceive[]) history;
+
     //Adding a name to wallet address
 
-function addName(string memory _name) public {
-
-    userName storage newUserName=names[msg.sender];
-    newUserName.name=_name;
-    newUserName.hasName=true;
-}
-
-
+    function addName(string memory _name) public {
+        userName storage newUserName = names[msg.sender];
+        newUserName.name = _name;
+        newUserName.hasName = true;
+    }
 
     //Creating a request
 
-function createRequest(address user,uint _amount,string memory _message) public{
+    function createRequest(
+        address user,
+        uint _amount,
+        string memory _message
+    ) public {
+        request memory newRequest;
+        newRequest.requestor = msg.sender;
+        newRequest.amount = _amount;
+        newRequest.message = _message;
 
-request memory newRequest;
-newRequest.requestor=msg.sender;
-newRequest.amount=_amount;
-newRequest.message=_message;
+        if (names[msg.sender].hasName) {
+            newRequest.name = names[msg.sender].name;
+        }
 
-if(names[msg.sender].hasName){
-    newRequest.name=names[msg.sender].name;
-}
-
-requests[user].push(newRequest);
-
-
-
-
-
-
-}
-
-
-
-
+        requests[user].push(newRequest);
+    }
 
     //Paying To Request
+
+
+    function payRequest(uint _request) public payable{
+
+    }
 
     //Get All all request send to user
 
