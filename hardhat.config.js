76e6c34124423467890c80aad44f23d9560efc4a
to4to <justjustifying@outlook.com>
2023-10-01 17:46:49 +0530

    deploy.js updated working on hardhat.congig.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,28 @@
-require("@nomicfoundation/hardhat-toolbox");
+
+
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+
+  networks:{
+
+mumbai:{
+
+  url:,
+  accounts: [process.env.PRIVATE_KEY],
+
+
+},
+
+  },
+
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,23 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const hre= require("hardhat");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+async function main(){
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
 
-  await lock.waitForDeployment();
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+
+const Paypal= await hre.ethers.getContractFactory("PayPal");
+const paypal= await Lock.deploy();
+
+
+await paypal.deployed(); 
+
+
+
+console.log("Paypal deployed to:", paypal.address);
+
+
+
+
+
 }
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
